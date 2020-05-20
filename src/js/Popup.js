e940f2a8a5f7756189bf2202ca0c361bc4f42837
Bohdan Hnatiuk <dmitrov.977@yandex.ru>
2020-05-20 18:00:50 +0300

    Настройка webpack и правка файла readme.md

---
+++ src/js/Popup.js
@@ -0,0 +1,11 @@
+export class Popup {
+  constructor(popupElem) {
+    this.popupElem = popupElem;
+  }
+  open() {
+    this.popupElem.classList.toggle('popup_is-opened');     
+  }
+  close() {
+    this.popupElem.classList.remove('popup_is-opened'); 
+  }    
+}
